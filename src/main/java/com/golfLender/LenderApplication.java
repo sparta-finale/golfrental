860ed0107a053d8384634293056c66289f0f69e3
moominIsCute <1happymoomin1@gmail.com>
2025-11-25 15:08:17 +0900

    first commit

---
+++ src/main/java/com/golfLender/LenderApplication.java
@@ -0,0 +1,13 @@
+package com.lender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/golfLender/LenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

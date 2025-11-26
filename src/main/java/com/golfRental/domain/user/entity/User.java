bc282df1ecb2be88552d85a5d5d3964ded31f422
Sungho <nanumeran@gmail.com>
2025-11-26 16:21:36 +0900

    init(user): User initial settings
    - UserEntity 구현

---
+++ src/main/java/com/golfRental/domain/user/entity/User.java
@@ -0,0 +1,58 @@
+package com.golfRental.domain.user.entity;
+
+import com.golfRental.common.entity.BaseEntity;
+import com.golfRental.domain.user.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 60)
+    private String email;
+
+    @Column(nullable = true, unique = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, unique = false, length = 60)
+    private String name;
+
+    @Column(nullable = true, unique = true, length = 30)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = false, length = 60)
+    private String address;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String nickname;
+
+    @Column(nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private User(
+            String email, String password, String name, String phoneNumber, String address, String nickname
+    ) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.nickname = nickname;
+        this.role = UserRole.ROLE_USER;
+    }
+
+    public static User create(
+            String email, String password, String name, String phoneNumber, String address, String nickname
+    ) {
+        return new User(email, password, name, phoneNumber, address, nickname);
+    }
+}

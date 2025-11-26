dae1b015aee18a0111b50fcf715b3b4152c868a2
Sungho <nanumeran@gmail.com>
2025-11-26 16:46:40 +0900

    init(user): User initial settings
    - 코드 리뷰 반영
    - 잘못된 import 수정

--- src/main/java/com/golfRental/domain/user/repository/UserRepository.java
+++ src/main/java/com/golfRental/domain/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.golfRental.domain.user.repository;
 
-import org.apache.catalina.User;
+import com.golfRental.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {

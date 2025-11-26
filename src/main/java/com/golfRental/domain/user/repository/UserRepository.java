70528a195267dfc0d8686aed69239d4800bf89bb
Sungho <nanumeran@gmail.com>
2025-11-26 16:21:47 +0900

    init(user): User initial settings
    - UserRepository 구현

---
+++ src/main/java/com/golfRental/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.golfRental.domain.user.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}

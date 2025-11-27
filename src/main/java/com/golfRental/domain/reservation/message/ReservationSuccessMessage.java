ddc48502a423cca871b13751396c880c23d791df
zerone <bazz0324@naver.com>
2025-11-28 03:36:37 +0900

    feat: add ReservationSuccessMessage constants
    - 예약 관련 성공 메시지 상수 클래스 생성
    - GET_RESERVATION 메시지 정의

---
+++ src/main/java/com/golfRental/domain/reservation/message/ReservationSuccessMessage.java
@@ -0,0 +1,9 @@
+package com.golfRental.domain.reservation.message;
+
+public final class ReservationSuccessMessage {
+
+    public final static String GET_RESERVATION = "예약 조회에 성공하였습니다.";
+
+    private ReservationSuccessMessage() {
+    }
+}

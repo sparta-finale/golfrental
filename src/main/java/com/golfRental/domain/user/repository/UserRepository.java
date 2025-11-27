a04fe3da44810befa67188d646e383f9020565a7
Sungho <nanumeran@gmail.com>
2025-11-27 09:42:32 +0900

    feat(user): User Signup API implementation
    - UserRepository 구현

--- src/main/java/com/golfRental/domain/user/repository/UserRepository.java
+++ src/main/java/com/golfRental/domain/user/repository/UserRepository.java
@@ -4,4 +4,9 @@ import com.golfRental.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByPhoneNumber(String phoneNumber);
 }

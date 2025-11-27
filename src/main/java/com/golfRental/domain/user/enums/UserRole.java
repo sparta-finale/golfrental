76f5bcc630a4dfeed0f0c36c5f297ac462d77ad6
Sungho <nanumeran@gmail.com>
2025-11-27 10:35:51 +0900

    refactor(user): User Signup API implementation
    - 일관성과 안정성을 위한 수정

--- src/main/java/com/golfRental/domain/user/enums/UserRole.java
+++ src/main/java/com/golfRental/domain/user/enums/UserRole.java
@@ -16,7 +16,7 @@ public enum UserRole {
 
     public static UserRole of(String role) {
         return Arrays.stream(UserRole.values())
-                .filter(r -> r.name().equalsIgnoreCase(role))
+                .filter(r -> r.getUserRole().equalsIgnoreCase(role))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 UserRole"));
     }

217304457c43189978bc53d16dfb894c1edd7a8b
Sungho <nanumeran@gmail.com>
2025-11-27 12:26:38 +0900

    feat(user): user login API implementation
    - 에러 코드 추가
    - 성공 메시지 추가

--- src/main/java/com/golfRental/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/com/golfRental/domain/auth/exception/AuthErrorCode.java
@@ -11,8 +11,11 @@ public enum AuthErrorCode implements ErrorCode {
 
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다."),
-    DUPLICATE_PHONE_NUMBER(HttpStatus.CONFLICT, "이미 사용 중인 전화번호입니다.");
-    
+    DUPLICATE_PHONE_NUMBER(HttpStatus.CONFLICT, "이미 사용 중인 전화번호입니다."),
+
+    INVALID_EMAIL(HttpStatus.BAD_REQUEST, "잘못된 이메일입니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+
     private final HttpStatus httpStatus;
     private final String message;
 }

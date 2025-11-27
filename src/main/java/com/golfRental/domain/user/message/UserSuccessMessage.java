8f197bcecc9a017e85c21dc6696fe2d14e69dbda
Sungho <nanumeran@gmail.com>
2025-11-27 17:41:48 +0900

    feat(user): Implementing My Information Inquiry API
    - UserSuccessMessage 구현

---
+++ src/main/java/com/golfRental/domain/user/message/UserSuccessMessage.java
@@ -0,0 +1,9 @@
+package com.golfRental.domain.user.message;
+
+public class UserSuccessMessage {
+
+    public final static String GET_MY_INFO = "내 정보 조회에 성공하였습니다.";
+
+    private UserSuccessMessage() {
+    }
+}

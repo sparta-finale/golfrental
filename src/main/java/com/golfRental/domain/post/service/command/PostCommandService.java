bca4774ea6a38d79832f6bb1dcf59066d9f24a7d
Sungho <nanumeran@gmail.com>
2025-11-28 02:13:43 +0900

    refactor(post): Post initial settings
    - findById 메서드 제거

--- src/main/java/com/golfRental/domain/post/service/command/PostCommandService.java
+++ src/main/java/com/golfRental/domain/post/service/command/PostCommandService.java
@@ -1,8 +1,4 @@
 package com.golfRental.domain.post.service.command;
 
-import com.golfRental.domain.post.entity.Post;
-
 public interface PostCommandService {
-
-    Post findById(Long postId);
 }
--- src/main/java/com/golfRental/domain/post/service/command/PostCommandServiceImpl.java
+++ src/main/java/com/golfRental/domain/post/service/command/PostCommandServiceImpl.java
@@ -1,8 +1,5 @@
 package com.golfRental.domain.post.service.command;
 
-import com.golfRental.domain.post.entity.Post;
-import com.golfRental.domain.post.exception.PostErrorCode;
-import com.golfRental.domain.post.exception.PostException;
 import com.golfRental.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +13,4 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostCommandServiceImpl implements PostCommandService {
 
     private final PostRepository postRepository;
-
-    @Override
-    public Post findById(Long postId) {
-        return postRepository.findById(postId).orElseThrow(
-                () -> new PostException(PostErrorCode.POST_INVALID_ID)
-        );
-    }
 }

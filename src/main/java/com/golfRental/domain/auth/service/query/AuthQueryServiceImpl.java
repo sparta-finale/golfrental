8aa86f9eb783a93723a32a66b8de9a8e890f3286
Sungho <nanumeran@gmail.com>
2025-11-27 12:27:10 +0900

    feat(user): user login API implementation
    - AuthService login 구현
    - 주석 추가

--- src/main/java/com/golfRental/domain/auth/service/query/AuthQueryServiceImpl.java
+++ src/main/java/com/golfRental/domain/auth/service/query/AuthQueryServiceImpl.java
@@ -1,7 +1,15 @@
 package com.golfRental.domain.auth.service.query;
 
+import com.golfRental.domain.auth.dto.request.AuthLoginRequest;
+import com.golfRental.domain.auth.dto.response.AuthLoginResponse;
+import com.golfRental.domain.auth.exception.AuthErrorCode;
+import com.golfRental.domain.auth.exception.AuthException;
+import com.golfRental.domain.user.entity.User;
+import com.golfRental.domain.user.service.query.UserQueryService;
+import com.golfRental.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +18,31 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AuthQueryServiceImpl implements AuthQueryService {
+
+    private final UserQueryService userQueryService;
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public AuthLoginResponse login(AuthLoginRequest authLoginRequest) {
+
+        if (!userQueryService.existsByEmail(authLoginRequest.getEmail())) {
+            log.warn("로그인 실패 - 잘못된 이메일 - email: {}", authLoginRequest.getEmail());
+            throw new AuthException(AuthErrorCode.INVALID_EMAIL);
+        }
+
+        User user = userQueryService.findByEmail(authLoginRequest.getEmail());
+
+        if (!passwordEncoder.matches(authLoginRequest.getPassword(), user.getPassword())) {
+            log.warn("로그인 실패 - 잘못된 비밀번호 - password: {}", authLoginRequest.getPassword());
+            throw new AuthException(AuthErrorCode.INVALID_PASSWORD);
+        }
+
+        String accessToken = jwtUtil.createToken(user.getId(), user.getRole());
+
+        AuthLoginResponse authLoginResponse = AuthLoginResponse.builder().accessToken(accessToken).build();
+
+        return authLoginResponse;
+    }
 }

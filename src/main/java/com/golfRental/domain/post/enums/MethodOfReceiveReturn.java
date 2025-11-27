25af2fd49089f87af44839cc6fbd94e52c6b267e
Sungho <nanumeran@gmail.com>
2025-11-28 01:44:13 +0900

    init(post): Post initial settings
    - Post Entity에서 사용되는 Enum 초기 세팅

---
+++ src/main/java/com/golfRental/domain/post/enums/MethodOfReceiveReturn.java
@@ -0,0 +1,7 @@
+package com.golfRental.domain.post.enums;
+
+public enum MethodOfReceiveReturn {
+
+    DELIVERY,               // 배송
+    DIRECT_TRANSACTION      // 직거래
+}
---
+++ src/main/java/com/golfRental/domain/post/enums/TradeStatus.java
@@ -0,0 +1,8 @@
+package com.golfRental.domain.post.enums;
+
+public enum TradeStatus {
+    
+    BEFORE_TRANSACTION,     // 거래 전
+    Trading,                // 거래 중
+    TRANSACTION_COMPLETED   // 거래 완료
+}

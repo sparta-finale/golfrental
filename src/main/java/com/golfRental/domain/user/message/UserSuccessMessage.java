d6512bb58320801984d7363903686e5fc6522cfc
Sungho <nanumeran@gmail.com>
2025-11-27 20:37:55 +0900

    feat(user): Implementing the Edit My Information API
    - User update 성공 메시지 구현

--- src/main/java/com/golfRental/domain/user/message/UserSuccessMessage.java
+++ src/main/java/com/golfRental/domain/user/message/UserSuccessMessage.java
@@ -3,6 +3,7 @@ package com.golfRental.domain.user.message;
 public final class UserSuccessMessage {
 
     public final static String GET_MY_INFO = "내 정보 조회에 성공하였습니다.";
+    public static final String UPDATE_MY_INFO = "내 정보 수정에 성공하였습니다.";
 
     private UserSuccessMessage() {
     }

ccbf989bff965874e5e7fa34ecb697885cc14079
zerone <bazz0324@naver.com>
2025-11-27 23:49:05 +0900

    init(category): Add Category entity
    - Category 엔티티 생성
    - name 필드에 unique 제약 및 길이 제한 설정
    - BaseEntity 상속 적용
    - Builder 패턴 도입을 위한 private 생성자 추가

---
+++ src/main/java/com/golfRental/domain/category/entity/Category.java
@@ -0,0 +1,27 @@
+package com.golfRental.domain.category.entity;
+
+import com.golfRental.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "categories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Builder
+    private Category(String name) {
+        this.name = name;
+    }
+}

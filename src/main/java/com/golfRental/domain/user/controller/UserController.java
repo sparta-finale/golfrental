a31d8223a38a47f6a5f4f6daf681db2e5f5b593e
Sungho <nanumeran@gmail.com>
2025-11-27 20:38:19 +0900

    feat(user): Implementing the Edit My Information API
    - User update Controller 구현

--- src/main/java/com/golfRental/domain/user/controller/UserController.java
+++ src/main/java/com/golfRental/domain/user/controller/UserController.java
@@ -2,19 +2,34 @@ package com.golfRental.domain.user.controller;
 
 import com.golfRental.common.response.CommonApiResponse;
 import com.golfRental.domain.auth.dto.AuthUser;
+import com.golfRental.domain.user.dto.request.UserUpdateMyInfoRequest;
 import com.golfRental.domain.user.dto.response.UserGetMyInfoResponse;
+import com.golfRental.domain.user.dto.response.UserUpdateMyInfoResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UserController {
 
     /**
      * 내 정보 조회
      *
-     * @param authUser
+     * @param authUser 유저 토큰 정보
      * @return UserGetMyInfoResponse
      */
     ResponseEntity<CommonApiResponse<UserGetMyInfoResponse>> getMyInfo(
             @AuthenticationPrincipal AuthUser authUser
+    );
+
+    /**
+     * 내 정보 수정
+     *
+     * @param authUser                유저 토큰 정보
+     * @param userUpdateMyInfoRequest 내 정보 수정사항
+     * @return UserUpdateMyInfoResponse
+     */
+    ResponseEntity<CommonApiResponse<UserUpdateMyInfoResponse>> updateMyInfo(
+            @AuthenticationPrincipal AuthUser authUser,
+            @RequestBody UserUpdateMyInfoRequest userUpdateMyInfoRequest
     );
 }
--- src/main/java/com/golfRental/domain/user/controller/UserControllerImpl.java
+++ src/main/java/com/golfRental/domain/user/controller/UserControllerImpl.java
@@ -2,15 +2,17 @@ package com.golfRental.domain.user.controller;
 
 import com.golfRental.common.response.CommonApiResponse;
 import com.golfRental.domain.auth.dto.AuthUser;
+import com.golfRental.domain.user.dto.request.UserUpdateMyInfoRequest;
 import com.golfRental.domain.user.dto.response.UserGetMyInfoResponse;
+import com.golfRental.domain.user.dto.response.UserUpdateMyInfoResponse;
 import com.golfRental.domain.user.message.UserSuccessMessage;
+import com.golfRental.domain.user.service.command.UserCommandService;
 import com.golfRental.domain.user.service.query.UserQueryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserControllerImpl implements UserController {
 
     private final UserQueryService userQueryService;
+    private final UserCommandService userCommandService;
 
     @Override
     @GetMapping("/users/me")
@@ -27,5 +30,18 @@ public class UserControllerImpl implements UserController {
         UserGetMyInfoResponse userGetMyInfoResponse = userQueryService.getMyInfo(authUser.getUserId());
 
         return CommonApiResponse.success(userGetMyInfoResponse, UserSuccessMessage.GET_MY_INFO);
+    }
+
+    @Override
+    @PutMapping("/users/me")
+    public ResponseEntity<CommonApiResponse<UserUpdateMyInfoResponse>> updateMyInfo(
+            @AuthenticationPrincipal AuthUser authUser,
+            @Valid @RequestBody UserUpdateMyInfoRequest userUpdateMyInfoRequest
+    ) {
+        UserUpdateMyInfoResponse userUpdateMyInfoResponse = userCommandService.updateMyInfo(
+                authUser.getUserId(), userUpdateMyInfoRequest
+        );
+
+        return CommonApiResponse.success(userUpdateMyInfoResponse, UserSuccessMessage.UPDATE_MY_INFO);
     }
 }

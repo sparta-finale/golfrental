9304ab047b4480a1823d98859307c1e33586ea6e
Sungho <nanumeran@gmail.com>
2025-11-27 18:07:21 +0900

    refactor(user): Implementing My Information Inquiry API
    - 상속 방지를 위한 final 추가
    - 문법을 조금 더 자연스럽게 수정

--- src/main/java/com/golfRental/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/golfRental/domain/user/exception/UserErrorCode.java
@@ -13,7 +13,7 @@ public enum UserErrorCode implements ErrorCode {
     USER_INVALID_EMAIL(HttpStatus.BAD_REQUEST, "해당 이메일을 가진 유저를 찾을 수 없습니다."),
 
     // findById, getMyInfo
-    USER_INVALID_ID(HttpStatus.BAD_REQUEST, "해당 ID을 가진 유저를 찾을 수 없습니다.");
+    USER_INVALID_ID(HttpStatus.BAD_REQUEST, "해당 ID를 가진 유저를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/golfRental/domain/user/message/UserSuccessMessage.java
+++ src/main/java/com/golfRental/domain/user/message/UserSuccessMessage.java
@@ -1,6 +1,6 @@
 package com.golfRental.domain.user.message;
 
-public class UserSuccessMessage {
+public final class UserSuccessMessage {
 
     public final static String GET_MY_INFO = "내 정보 조회에 성공하였습니다.";
 

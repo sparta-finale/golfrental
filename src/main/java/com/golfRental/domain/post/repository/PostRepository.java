5a82a54c9c086bc9a3b0e170e0c7705e1ffa7ec9
Sungho <nanumeran@gmail.com>
2025-11-29 04:42:26 +0900

    feat(post): Implementing a post list query API
    - 게시물 목록 조회 Service 구현

--- src/main/java/com/golfRental/domain/post/repository/PostRepository.java
+++ src/main/java/com/golfRental/domain/post/repository/PostRepository.java
@@ -1,7 +1,22 @@
 package com.golfRental.domain.post.repository;
 
 import com.golfRental.domain.post.entity.Post;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("""
+                    SELECT p FROM Post p
+                    ORDER BY
+                        CASE p.tradeStatus
+                            WHEN 'BEFORE_TRANSACTION' THEN 0
+                            WHEN 'TRADING' THEN 1
+                            WHEN 'TRANSACTION_COMPLETED' THEN 2
+                        END ASC,
+                        p.createdAt DESC
+            """)
+    Slice<Post> findAllOrderByStatus(Pageable pageable);
 }

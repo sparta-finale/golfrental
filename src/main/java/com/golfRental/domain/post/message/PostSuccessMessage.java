89bb586add4a537b6b1c2fad82decd89aa2104b2
Sungho <nanumeran@gmail.com>
2025-11-29 04:41:50 +0900

    feat(post): Implementing a post list query API
    - 게시물 목록 조회에 따른 성공 메시지 추가

--- src/main/java/com/golfRental/domain/post/message/PostSuccessMessage.java
+++ src/main/java/com/golfRental/domain/post/message/PostSuccessMessage.java
@@ -3,6 +3,7 @@ package com.golfRental.domain.post.message;
 public final class PostSuccessMessage {
 
     public final static String POST_CREATED = "게시물이 생성되었습니다.";
+    public final static String POST_GET_ALL = "게시물 목록이 조회되었습니다.";
 
     private PostSuccessMessage() {
     }

63fe2214a4714bb0a1630e5feb0ca7ffd24db7f8
Sungho <nanumeran@gmail.com>
2025-11-29 04:42:35 +0900

    feat(post): Implementing a post list query API
    - 게시물 목록 조회 Controller 구현

--- src/main/java/com/golfRental/domain/post/controller/PostController.java
+++ src/main/java/com/golfRental/domain/post/controller/PostController.java
@@ -1,18 +1,31 @@
 package com.golfRental.domain.post.controller;
 
 import com.golfRental.common.response.CommonApiResponse;
+import com.golfRental.common.response.SliceResponse;
 import com.golfRental.domain.auth.dto.AuthUser;
 import com.golfRental.domain.post.dto.request.PostCreateRequest;
 import com.golfRental.domain.post.dto.response.PostCreateResponse;
+import com.golfRental.domain.post.dto.response.PostGetAllResponse;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface PostController {
 
     ResponseEntity<CommonApiResponse<PostCreateResponse>> createPost(
             @AuthenticationPrincipal AuthUser authUser,
             @Valid @RequestBody PostCreateRequest postCreateRequest
+    );
+
+    ResponseEntity<CommonApiResponse<SliceResponse<PostGetAllResponse>>> getAll(
+            // 카테고리 추가 예정
+
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam int size,
+            @RequestParam(defaultValue = "createdAt") String sort,
+            @RequestParam(defaultValue = "DESC") Sort.Direction direction
     );
 }
--- src/main/java/com/golfRental/domain/post/controller/PostControllerImpl.java
+++ src/main/java/com/golfRental/domain/post/controller/PostControllerImpl.java
@@ -1,20 +1,23 @@
 package com.golfRental.domain.post.controller;
 
 import com.golfRental.common.response.CommonApiResponse;
+import com.golfRental.common.response.SliceResponse;
 import com.golfRental.domain.auth.dto.AuthUser;
 import com.golfRental.domain.post.dto.request.PostCreateRequest;
 import com.golfRental.domain.post.dto.response.PostCreateResponse;
+import com.golfRental.domain.post.dto.response.PostGetAllResponse;
 import com.golfRental.domain.post.message.PostSuccessMessage;
 import com.golfRental.domain.post.service.command.PostCommandService;
+import com.golfRental.domain.post.service.query.PostQueryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -26,6 +29,7 @@ import java.net.URI;
 public class PostControllerImpl implements PostController {
 
     private final PostCommandService postCommandService;
+    private final PostQueryService postQueryService;
 
     @Override
     @PostMapping("/posts")
@@ -43,5 +47,22 @@ public class PostControllerImpl implements PostController {
 
         return ResponseEntity.created(location)
                 .body(CommonApiResponse.created(postCreateResponse, PostSuccessMessage.POST_CREATED).getBody());
+    }
+
+    @Override
+    @GetMapping("/posts")
+    public ResponseEntity<CommonApiResponse<SliceResponse<PostGetAllResponse>>> getAll(
+            // 카테고리 추가 예정
+
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size,
+            @RequestParam(defaultValue = "createdAt") String sort,
+            @RequestParam(defaultValue = "DESC") Sort.Direction direction
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sort));
+
+        SliceResponse<PostGetAllResponse> posts = postQueryService.getAll(pageable);
+
+        return CommonApiResponse.sliceSuccess(posts, PostSuccessMessage.POST_GET_ALL);
     }
 }

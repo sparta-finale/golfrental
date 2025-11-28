d59920d18dd8a37bbcf7890f1788e209235398bc
Sungho <nanumeran@gmail.com>
2025-11-29 04:42:15 +0900

    feat(post): Implementing a post list query API
    - 게시물 목록 조회 Service 구현

--- src/main/java/com/golfRental/domain/post/service/query/PostQueryService.java
+++ src/main/java/com/golfRental/domain/post/service/query/PostQueryService.java
@@ -1,8 +1,13 @@
 package com.golfRental.domain.post.service.query;
 
+import com.golfRental.common.response.SliceResponse;
+import com.golfRental.domain.post.dto.response.PostGetAllResponse;
 import com.golfRental.domain.post.entity.Post;
+import org.springframework.data.domain.Pageable;
 
 public interface PostQueryService {
+
+    SliceResponse<PostGetAllResponse> getAll(Pageable pageable);
 
     Post findById(Long postId);
 }
--- src/main/java/com/golfRental/domain/post/service/query/PostQueryServiceImpl.java
+++ src/main/java/com/golfRental/domain/post/service/query/PostQueryServiceImpl.java
@@ -1,13 +1,19 @@
 package com.golfRental.domain.post.service.query;
 
+import com.golfRental.common.response.SliceResponse;
+import com.golfRental.domain.post.dto.response.PostGetAllResponse;
 import com.golfRental.domain.post.entity.Post;
 import com.golfRental.domain.post.exception.PostErrorCode;
 import com.golfRental.domain.post.exception.PostException;
 import com.golfRental.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -16,6 +22,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostQueryServiceImpl implements PostQueryService {
 
     private final PostRepository postRepository;
+
+    @Override
+    public SliceResponse<PostGetAllResponse> getAll(Pageable pageable) {
+
+        Slice<Post> posts = postRepository.findAllOrderByStatus(pageable);
+
+        List<PostGetAllResponse> content = posts.stream()
+                .map(post -> PostGetAllResponse.builder()
+                        .id(post.getId())
+                        .title(post.getTitle())
+                        .content(post.getContent())
+                        .methodOfReceive(post.getMethodOfReceive())
+                        .methodOfReturn(post.getMethodOfReturn())
+                        .price(post.getPrice())
+                        .deposit(post.getDeposit())
+                        .dailyRate(post.getDailyRate())
+                        .tradeStatus(post.getTradeStatus())
+                        .userId(post.getUser().getId())
+                        .username(post.getUser().getUsername())
+                        .address(post.getUser().getAddress())
+                        .nickname(post.getUser().getNickname())
+                        .build()
+                ).toList();
+
+        return new SliceResponse<>(content, posts.getSize(), posts.getNumber(), posts.hasNext());
+    }
 
     @Override
     public Post findById(Long postId) {

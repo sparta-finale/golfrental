0c566643d6f19d4360a717c16ea68d5f511a40c3
Sungho <nanumeran@gmail.com>
2025-11-28 09:55:42 +0900

    fix(auth): Remove anonymous code

--- src/main/java/com/golfRental/security/config/SecurityConfig.java
+++ src/main/java/com/golfRental/security/config/SecurityConfig.java
@@ -40,7 +40,6 @@ public class SecurityConfig {
 
                 // JWT 사용 시 불필요한 기능들 비활성화
                 .formLogin(AbstractHttpConfigurer::disable)      // [SSR] 서버가 로그인 HTML 폼 렌더링
-                .anonymous(AbstractHttpConfigurer::disable)      // 미인증 사용자를 익명으로 처리
                 .httpBasic(AbstractHttpConfigurer::disable)      // [SSR] 인증 팝업
                 .logout(AbstractHttpConfigurer::disable)         // [SSR] 서버가 세션 무효화 후 리다이렉트
                 .rememberMe(AbstractHttpConfigurer::disable)     // 서버가 쿠키 발급하여 자동 로그인

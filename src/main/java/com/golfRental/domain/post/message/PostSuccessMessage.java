eed99de7abe80bfd9741c50b57614b0f4f03d535
Sungho <nanumeran@gmail.com>
2025-11-28 20:23:17 +0900

    feat(post): Post registration API implementation
    - 성공 메시지 구현

---
+++ src/main/java/com/golfRental/domain/post/message/PostSuccessMessage.java
@@ -0,0 +1,9 @@
+package com.golfRental.domain.post.message;
+
+public final class PostSuccessMessage {
+
+    public final static String POST_CREATED = "게시물이 생성되었습니다.";
+
+    private PostSuccessMessage() {
+    }
+}

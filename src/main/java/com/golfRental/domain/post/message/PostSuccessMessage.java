54933491202314b3d39c0d508a86ab2793a403c8
Sungho <nanumeran@gmail.com>
2025-11-29 17:25:13 +0900

    feat(post): Implementing my post search API
    - 나의 게시물 조회 응답 DTO 구현
    - 나의 게시물 조회 성공 메시지 추가

--- src/main/java/com/golfRental/domain/post/message/PostSuccessMessage.java
+++ src/main/java/com/golfRental/domain/post/message/PostSuccessMessage.java
@@ -5,6 +5,7 @@ public final class PostSuccessMessage {
     public final static String POST_CREATED = "게시물이 생성되었습니다.";
     public final static String POST_GET_ALL = "게시물 목록이 조회되었습니다.";
     public final static String POST_GETS = "게시물 상세 조회에 성공하었습니다.";
+    public final static String POST_GET_MY = "내 게시물 조회에 성공하었습니다.";
 
     private PostSuccessMessage() {
     }

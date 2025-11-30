ab8b95698009f41628ef0e67e160ee6f75d77821
moominIsCute <1happymoomin1@gmail.com>
2025-11-30 11:19:36 +0900

    refactor(review): used findByIdWithDetails

--- src/main/java/com/golfRental/domain/review/service/query/ReviewQueryServiceImpl.java
+++ src/main/java/com/golfRental/domain/review/service/query/ReviewQueryServiceImpl.java
@@ -5,16 +5,23 @@ import com.golfRental.domain.review.entity.Review;
 import com.golfRental.domain.review.exception.ReviewErrorCode;
 import com.golfRental.domain.review.exception.ReviewException;
 import com.golfRental.domain.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ReviewQueryServiceImpl implements ReviewQueryService {
 
-    ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
     @Override
     public ReviewResponse getReview(Long reviewId) {
-        Review review = reviewRepository.findById(reviewId)
+
+        Review review = reviewRepository.findByIdWithDetails(reviewId)
                 .orElseThrow(() -> new ReviewException(ReviewErrorCode.REVIEW_NOT_FOUND));
 
         return ReviewResponse.from(review);

0f9208b405838b8e2cbd31fc00f628b2408b879d
Sungho <nanumeran@gmail.com>
2025-11-30 16:09:54 +0900

    feat(post): Logout API implementation
    - 로그아웃 Controller 구현

--- src/main/java/com/golfRental/domain/auth/controller/AuthController.java
+++ src/main/java/com/golfRental/domain/auth/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.golfRental.domain.auth.controller;
 
 import com.golfRental.common.response.CommonApiResponse;
+import com.golfRental.domain.auth.dto.AuthUser;
 import com.golfRental.domain.auth.dto.request.AuthLoginRequest;
 import com.golfRental.domain.auth.dto.request.AuthSignupRequest;
 import com.golfRental.domain.auth.dto.response.AuthLoginResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AuthController {
@@ -13,7 +15,7 @@ public interface AuthController {
     /**
      * 회원가입
      *
-     * @param authSignupRequest
+     * @param authSignupRequest 회원가입 시 필요 데이터
      * @return void
      */
     ResponseEntity<CommonApiResponse<Void>> signup(
@@ -23,10 +25,20 @@ public interface AuthController {
     /**
      * 로그인
      *
-     * @param authLoginRequest
-     * @return 토큰
+     * @param authLoginRequest 로그인 시 필요 데이터
+     * @return AuthLoginResponse
      */
     ResponseEntity<CommonApiResponse<AuthLoginResponse>> login(
             @Valid @RequestBody AuthLoginRequest authLoginRequest
+    );
+
+    /**
+     * 로그아웃
+     *
+     * @param authUser 토큰 정보
+     * @return void
+     */
+    ResponseEntity<CommonApiResponse<Void>> logout(
+            @AuthenticationPrincipal AuthUser authUser
     );
 }

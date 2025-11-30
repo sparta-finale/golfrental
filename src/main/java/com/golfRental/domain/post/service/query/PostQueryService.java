2c881de0bd30d8de0c25104254071f6119ff1220
Sungho <nanumeran@gmail.com>
2025-11-30 14:49:35 +0900

    feat(post): Implementing an API to retrieve a list of posts by category
    - 카테고리를 통한 게시물 조회 Service 구현

--- src/main/java/com/golfRental/domain/post/service/query/PostQueryService.java
+++ src/main/java/com/golfRental/domain/post/service/query/PostQueryService.java
@@ -2,6 +2,7 @@ package com.golfRental.domain.post.service.query;
 
 import com.golfRental.common.response.SliceResponse;
 import com.golfRental.domain.post.dto.response.PostGetAllResponse;
+import com.golfRental.domain.post.dto.response.PostGetByCategoryResponse;
 import com.golfRental.domain.post.dto.response.PostGetMyResponse;
 import com.golfRental.domain.post.dto.response.PostGetsResponse;
 import com.golfRental.domain.post.entity.Post;
@@ -33,6 +34,15 @@ public interface PostQueryService {
      * @return SliceResponse<PostGetMyResponse>
      */
     SliceResponse<PostGetMyResponse> getMyPost(Long userId, Pageable pageable);
+
+    /**
+     * 카테고리를 통한 게시물 조회 API
+     *
+     * @param categoryId 카테고리 ID
+     * @param pageable   페이지 데이터
+     * @return SliceResponse<PostGetByCategoryResponse>
+     */
+    SliceResponse<PostGetByCategoryResponse> getByCategory(Long categoryId, Pageable pageable);
 
     Post findById(Long postId);
 }

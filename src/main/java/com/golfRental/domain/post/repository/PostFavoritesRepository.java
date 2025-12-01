5c8149d96540179f547f8c2f11e650b8bd516af4
Sungho <nanumeran@gmail.com>
2025-12-01 17:01:17 +0900

    feat(post): Implementing the Post Favorites View API
    - 즐겨찾기를 통한 조회 Repositroy 구현

--- src/main/java/com/golfRental/domain/post/repository/PostFavoritesRepository.java
+++ src/main/java/com/golfRental/domain/post/repository/PostFavoritesRepository.java
@@ -3,6 +3,8 @@ package com.golfRental.domain.post.repository;
 import com.golfRental.domain.post.entity.Post;
 import com.golfRental.domain.post.entity.PostFavorites;
 import com.golfRental.domain.user.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,13 @@ public interface PostFavoritesRepository extends JpaRepository<PostFavorites, Lo
 
     @Query("SELECT pf.post.id FROM PostFavorites pf WHERE pf.user = :user")
     Set<Long> findPostIdsByUser(@Param("user") User user);
+
+    @Query("""
+            SELECT pf
+            FROM PostFavorites pf
+            JOIN FETCH pf.post p
+            JOIN FETCH pf.user u
+            WHERE pf.user = :user
+            """)
+    Slice<PostFavorites> findByUserWithPostAndUser(@Param("user") User user, Pageable pageable);
 }

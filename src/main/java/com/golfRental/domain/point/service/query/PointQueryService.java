a5f3859b5f669cdb5796263a9f1ea6b6247dccb8
zerone <bazz0324@naver.com>
2025-12-04 03:34:12 +0900

    refactor(point): Remove PointQueryService in query package
    - query 패키지의 PointQueryService를 제거하고 command 서비스 구조로 전환

--- src/main/java/com/golfRental/domain/point/service/query/PointQueryService.java
+++
@@ -1,8 +0,0 @@
-package com.golfRental.domain.point.service.query;
-
-import com.golfRental.domain.point.dto.response.PointBalanceResponse;
-
-public interface PointQueryService {
-
-    PointBalanceResponse getBalance(Long userId);
-}

ae75ed39e336ff3bb5dc06001bc1485508775591
moominIsCute <1happymoomin1@gmail.com>
2025-12-04 16:44:27 +0900

    refactor(redis): Add Redis configuration with type-safe deserialization

--- src/main/java/com/golfRental/common/config/RedisConfig.java
+++ src/main/java/com/golfRental/common/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.golfRental.common.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -22,7 +23,11 @@ public class RedisConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
         Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
 
         template.setKeySerializer(new StringRedisSerializer());

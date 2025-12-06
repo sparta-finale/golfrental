07abb5c7fecd9ef008fa609cb4595b4b5ca4dbc4
Sungho <nanumeran@gmail.com>
2025-12-06 20:53:27 +0900

    refactor(image): Image initial settings
    - 성공 메시지 접근 지정자 변경

--- src/main/java/com/golfRental/domain/image/message/ImageSuccessMessage.java
+++ src/main/java/com/golfRental/domain/image/message/ImageSuccessMessage.java
@@ -2,7 +2,7 @@ package com.golfRental.domain.image.message;
 
 public class ImageSuccessMessage {
 
-    private final static String IMAGE_PRESIGNED_URL_CREATED = "Pre-signed URL이 생성되었습니다.";
+    public final static String IMAGE_PRESIGNED_URL_CREATED = "Pre-signed URL이 생성되었습니다.";
 
     private ImageSuccessMessage() {
     }

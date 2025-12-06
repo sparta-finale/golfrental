2dabdb8653711426e2ff5f4e593e3ae18938a20c
Sungho <nanumeran@gmail.com>
2025-12-06 20:36:02 +0900

    init(image): Image initial settings
    - image Entity 구현

---
+++ src/main/java/com/golfRental/domain/image/entity/Image.java
@@ -0,0 +1,56 @@
+package com.golfRental.domain.image.entity;
+
+import com.golfRental.common.entity.BaseEntity;
+import com.golfRental.domain.image.enums.ImageType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "images")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String url;
+
+    @Column(length = 255, nullable = false)
+    private String fileName;
+
+    @Column(length = 1000, nullable = false)
+    private String s3Key;
+
+    @Column(length = 100, nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private Long size;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ImageType type;
+
+    @Builder
+    private Image(
+            String url,
+            String fileName,
+            String s3Key,
+            String contentType,
+            Long size,
+            ImageType type
+    ) {
+        this.url = url;
+        this.fileName = fileName;
+        this.s3Key = s3Key;
+        this.contentType = contentType;
+        this.size = size;
+        this.type = type;
+    }
+}

5c356fe3c91104411d9c48172fc293370c91c468
Sungho <nanumeran@gmail.com>
2025-12-07 13:14:22 +0900

    feat(image): Implementing image metadata storage
    - 이미지 메타데이터 저장 요청, 응답 Dto 구현

---
+++ src/main/java/com/golfRental/domain/image/dto/request/ImageSaveRequest.java
@@ -0,0 +1,29 @@
+package com.golfRental.domain.image.dto.request;
+
+import com.golfRental.domain.image.enums.ImageType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class ImageSaveRequest {
+    @NotBlank(message = "파일명은 필수입니다.")
+    private String fileName;
+
+    @NotBlank(message = "URL은 필수입니다.")
+    private String url;
+
+    @NotBlank(message = "S3 Key는 필수입니다.")
+    private String s3Key;
+
+    @NotBlank(message = "Content Type은 필수입니다.")
+    private String contentType;
+
+    @NotNull(message = "파일 크기는 필수입니다.")
+    @Min(value = 1, message = "파일 크기는 1바이트 이상이어야 합니다.")
+    private Long size;
+
+    @NotNull(message = "이미지 타입은 필수입니다.(현재는 post만 가능)")
+    private ImageType type;
+}

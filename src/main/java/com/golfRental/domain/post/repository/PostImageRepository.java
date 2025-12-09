6e594d5dbea1bcb5396871c1d48855d806fcac48
Sungho <nanumeran@gmail.com>
2025-12-10 00:35:47 +0900

    feat(post): Implementing a post image deletion API
    - 불필요한 로직 삭제 및 DeletedAtIsNull 조건 추가

--- src/main/java/com/golfRental/domain/post/repository/PostImageRepository.java
+++ src/main/java/com/golfRental/domain/post/repository/PostImageRepository.java
@@ -4,21 +4,8 @@ import com.golfRental.domain.image.entity.Image;
 import com.golfRental.domain.post.entity.Post;
 import com.golfRental.domain.post.entity.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 
-    @Query("""
-            SELECT pi FROM PostImage pi
-                JOIN FETCH pi.post p
-                JOIN FETCH pi.image i
-            WHERE p.deletedAt IS NULL
-                AND i.deletedAt IS NULL
-            ORDER BY pi.sortOrder ASC
-            """)
-    List<PostImage> findAllByIdWithDetail();
-
-    boolean existsByPostAndImage(Post post, Image image);
+    boolean existsByPostAndImageAndDeletedAtIsNull(Post post, Image image);
 }

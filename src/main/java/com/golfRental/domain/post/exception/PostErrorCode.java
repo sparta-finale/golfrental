1424f970448dd5b3e84b2978adf4cc0b8a06baa7
Sungho <nanumeran@gmail.com>
2025-12-10 00:34:53 +0900

    feat(post): Implementing a post image deletion API
    - 게시물 이미지 삭제 에러 코드 추가
    - 게시물 이미지 삭제 성공 코드 추가

--- src/main/java/com/golfRental/domain/post/exception/PostErrorCode.java
+++ src/main/java/com/golfRental/domain/post/exception/PostErrorCode.java
@@ -12,7 +12,7 @@ public enum PostErrorCode implements ErrorCode {
     // findById
     POST_INVALID_ID(HttpStatus.NOT_FOUND, "해당 ID를 가진 게시물을 찾을 수 없습니다."),
 
-    // updatePost
+    // updatePost, deleteImages
     POST_NOT_EQUAL_CREATOR(HttpStatus.FORBIDDEN, "게시물의 생성자만이 수정을 할 수 있습니다."),
 
     // addFavorites
@@ -25,7 +25,10 @@ public enum PostErrorCode implements ErrorCode {
     DUPLICATE_IMAGE_IDS(HttpStatus.BAD_REQUEST, "중복된 이미지 ID가 포함되어 있습니다."),
 
     // updateThumbnail
-    POST_IMAGE_NOT_EXIST(HttpStatus.BAD_REQUEST, "게시물에 이미지가 존재하지 않습니다.");
+    POST_IMAGE_NOT_EXIST(HttpStatus.BAD_REQUEST, "게시물에 이미지가 존재하지 않습니다."),
+
+    // deleteImages
+    POST_IMAGE_NEED(HttpStatus.BAD_REQUEST, "게시물에 하나 이상의 이미지 있어야 합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

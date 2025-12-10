1bd2a4b6cb37881786a95290abc17df8bc06284b
zerone <bazz0324@naver.com>
2025-12-11 05:15:54 +0900

    feat(payment): Add success message for payment history retrieval
    - PAYMENT_HISTORY_LIST "결제 내역 조회에 성공하였습니다." 메시지 추가

--- src/main/java/com/golfRental/domain/payment/message/PaymentSuccessMessage.java
+++ src/main/java/com/golfRental/domain/payment/message/PaymentSuccessMessage.java
@@ -3,6 +3,7 @@ package com.golfRental.domain.payment.message;
 public class PaymentSuccessMessage {
 
     public static final String PAYMENT_CONFIRMED = "결제 승인이 완료되었습니다.";
+    public static final String PAYMENT_HISTORY_LIST = "결제 내역 조회에 성공하였습니다.";
 
     private PaymentSuccessMessage() {
     }

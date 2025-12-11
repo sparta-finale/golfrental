9a0188e3a519487a304e26cc350abfb930ee6980
moominIsCute <1happymoomin1@gmail.com>
2025-12-11 11:19:45 +0900

    feat(review): add swaggerendpoint

--- src/main/java/com/golfRental/domain/chat/controller/ChatController.java
+++ src/main/java/com/golfRental/domain/chat/controller/ChatController.java
@@ -7,31 +7,93 @@ import com.golfRental.domain.chat.dto.request.ChatMessageCreateRequest;
 import com.golfRental.domain.chat.dto.request.ChatRoomCreateRequest;
 import com.golfRental.domain.chat.dto.response.ChatMessageResponse;
 import com.golfRental.domain.chat.dto.response.ChatRoomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "채팅 관리", description = "채팅 관련 API")
 public interface ChatController {
+
+    @Operation(
+            summary = "채팅방 생성",
+            description = "채팅방을 생성합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<ChatRoomResponse>> createChatRoom(
             AuthUser authUser,
             ChatRoomCreateRequest request
     );
 
+    @Operation(
+            summary = "채티방 조회",
+            description = "나의 채팅방을 조회합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<SliceResponse<ChatRoomResponse>>> getMyChatRooms(
             AuthUser authUser,
             Pageable pageable
     );
 
+    @Operation(
+            summary = "채팅방 조회",
+            description = "채팅방을 상세조회합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<ChatRoomResponse>> getChatRoom(
             AuthUser authUser,
             Long chatRoomId
     );
 
+
+    @Operation(
+            summary = "메세지 전송",
+            description = "메세지를 전송합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<ChatMessageResponse>> sendMessage(
             AuthUser authUser,
             Long chatRoomId,
             ChatMessageCreateRequest request
     );
 
+    @Operation(
+            summary = "메세지 조회",
+            description = "메세지를 조회합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<SliceResponse<ChatMessageResponse>>> getMessages(
             AuthUser authUser,
             Long chatRoomId,
--- src/main/java/com/golfRental/domain/review/controller/ReviewController.java
+++ src/main/java/com/golfRental/domain/review/controller/ReviewController.java
@@ -7,33 +7,93 @@ import com.golfRental.domain.review.dto.request.ReviewCreateRequest;
 import com.golfRental.domain.review.dto.request.ReviewUpdateRequest;
 import com.golfRental.domain.review.dto.response.ReviewGetResponse;
 import com.golfRental.domain.review.dto.response.ReviewResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "리뷰 관리", description = "리뷰 관련 API")
 public interface ReviewController {
 
+    @Operation(
+            summary = "리뷰 생성",
+            description = "새로운 리뷰를 등록합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<ReviewResponse>> createReview(
             AuthUser authUser,
             ReviewCreateRequest request
     );
 
+    @Operation(
+            summary = "리뷰 상세 조회",
+            description = "리뷰를 조회합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<ReviewResponse>> getReview(
             AuthUser authUser,
             Long reviewId
     );
 
+    @Operation(
+            summary = "리뷰 조회",
+            description = "유저의 리뷰를 조회합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<SliceResponse<ReviewGetResponse>>> getReviewsByTargetUser(
             AuthUser authUser,
             Long targetUserId,
             Pageable pageable
     );
 
+    @Operation(
+            summary = "리뷰 수정",
+            description = "리뷰를 수정합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<ReviewResponse>> updateReview(
             AuthUser authUser,
             Long reviewId,
             ReviewUpdateRequest request
     );
 
+    @Operation(
+            summary = "리뷰 삭제",
+            description = "리뷰를 삭제합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
+            }
+    )
     ResponseEntity<CommonApiResponse<Void>> deleteReview(
             AuthUser authUser,
             Long reviewId

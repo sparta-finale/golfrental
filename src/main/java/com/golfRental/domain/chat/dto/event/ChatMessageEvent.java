5c7c09dcacd9e30851471a49898a290d1041cd5a
moominIsCute <1happymoomin1@gmail.com>
2025-12-16 10:44:30 +0900

    refactor(chat): Remove unnecessary Serializable implementation from ChatMessageEvent

--- src/main/java/com/golfRental/domain/chat/dto/event/ChatMessageEvent.java
+++ src/main/java/com/golfRental/domain/chat/dto/event/ChatMessageEvent.java
@@ -2,15 +2,10 @@ package com.golfRental.domain.chat.dto.event;
 
 import com.golfRental.domain.chat.dto.response.ChatMessageResponse;
 
-import java.io.Serializable;
-
 public record ChatMessageEvent(
         Long chatRoomId,
         ChatMessageResponse message
-) implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
+) {
     public static ChatMessageEvent of(Long chatRoomId, ChatMessageResponse message) {
         return new ChatMessageEvent(chatRoomId, message);
     }
